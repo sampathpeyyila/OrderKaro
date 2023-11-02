26e30b700468de1adc58398be946e55eed87a481
sampathpeyyila <peyyilasampath@gmail.com>
2023-11-02 08:28:36 +0530

    first commit

---
+++ src/main/java/com/orderkaro/OrderKaroApplication.java
@@ -0,0 +1,13 @@
+package com.orderkaro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderKaroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderKaroApplication.class, args);
+	}
+
+}
